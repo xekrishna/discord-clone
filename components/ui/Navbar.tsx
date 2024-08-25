f66bbe55652a08c93b4d079f283a9b2f3fa19ed5
Mayank Bisht <octpahadi@gmail.com>
2024-08-25 15:02:37 +0530

    really important change tbh

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <main className="flex flex-col  h-[94vh] gap-1 max-h-[100vh]  max-w-20 w-20 justify-center items-center">
         
         <section className="flex-1 bg-bg4 h-full w-[90%] max-w-[90%] p-2 rounded-xl">
-            <div className="h-max w-full flex items-center justify-center bg-bg1">
+            <div className="h-max w-full flex items-center justify-center">
                 <BsDiscord className="text-3xl"/>
             </div>
             <div>
