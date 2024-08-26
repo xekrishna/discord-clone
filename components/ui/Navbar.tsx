d2ff00ed85856a169aae5a6733c9eebf21182553
Mayank Bisht <octpahadi@gmail.com>
2024-08-26 14:41:56 +0530

    added labels ( tried )

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,15 +1,17 @@
+
 import Image from "next/image";
-import { userData, serverData, PlusIcon, DiscordIcon } from '@/constants/data'
+import { userData, serverData, PlusIcon, DiscordIcon } from "@/constants/data";
 
 const iconProps =
   "h-12 w-12 transition-all hover:text-white hover:rounded-lg rounded-3xl duration-300 cursor-pointer";
+const labelProps = "absolute left-1/2 transform -translate-x-1/2 top-full mt-2 px-2 py-1 text-sm text-text3 bg-bg3 rounded opacity-0 group-hover:opacity-100 group-hover:block transition-all duration-300 z-50"
 
 const Navbar = () => {
   return (
     <main className="flex flex-col h-[94vh] gap-1 max-h-[100vh] max-w-20 w-20 justify-center items-center">
-      <section className="flex-1 bg-bg4 h-full w-[90%] max-w-[90%] p-2 rounded-xl overflow-y-auto hide-scrollbar">
+      <section className="relative flex-1 bg-bg4 h-full w-[90%] max-w-[90%] p-2 rounded-xl overflow-y-auto hide-scrollbar">
         <div className="h-max w-full flex items-center justify-center flex-col gap-2">
-          {/* Top section with Discord icon */}
+          {/* Top section */}
           <div className="bg-bg1 w-12 h-12 justify-center items-center flex rounded-full">
             <DiscordIcon className="text-3xl" />
           </div>
@@ -20,7 +22,7 @@ const Navbar = () => {
               <Image
                 key={icon.id}
                 src={icon.src}
-                alt={icon.alt}
+                alt='bleh'
                 className={iconProps}
                 width={48}
                 height={48}
@@ -32,16 +34,24 @@ const Navbar = () => {
           <div className="h-1 w-[80%] rounded-full bg-bg3"></div>
 
           {/* Servers */}
-          <div className="flex gap-3 flex-col">
+          <div className="flex gap-3 flex-col relative">
             {serverData.map((icon) => (
-              <Image
+              <div
                 key={icon.id}
-                src={icon.src}
-                alt={icon.alt}
-                className={iconProps}
-                width={48}
-                height={48}
-              />
+                className="relative flex items-center bg-black"
+              >
+                <Image
+                  src={icon.src}
+                  alt='bleh'
+                  className={iconProps}
+                  width={48}
+                  height={48}
+                />
+                {/* Label Display */}
+                <span className={labelProps}>
+                  {icon.serverName}
+                </span>
+              </div>
             ))}
           </div>
 
--- constants/data.ts
+++ constants/data.ts
@@ -1,27 +1,155 @@
 import { BsDiscord } from "react-icons/bs";
 import { PiPlus } from "react-icons/pi";
 
-type IconData = {
+interface User  {
   id: number;
   src: string;
-  alt: string;
+  username: string;
+  usertag: string;
 };
 
-export const userData: IconData[] = [
-  { id: 1, src: "/avatars/1.jpg", alt: "user" },
-  { id: 2, src: "/avatars/2.jpg", alt: "user" },
+interface Server  {
+  id: number;
+  src: string;
+  serverName: string;
+  categories: Categories[]
+};
+
+interface Categories {
+  name: string;
+  channels: Channels[]
+}
+
+interface Channels {
+  id: number;
+  name: string;
+  description?: string;
+  messages: string
+}
+
+export const userData: User[] = [
+  { id: 1, src: "/avatars/1.jpg", username: 'Sanzu', usertag: '@xesanzu' },
+  { id: 2, src: "/avatars/2.jpg", username:'Rumi', usertag: "@xerumi"},
 ];
 
-export const serverData: IconData[] = [
-  { id: 1, src: "/avatars/1.jpg", alt: "user" },
-  { id: 2, src: "/avatars/2.jpg", alt: "user" },
-  { id: 3, src: "/avatars/3.jpg", alt: "user" },
-  { id: 4, src: "/avatars/4.jpg", alt: "user" },
-  { id: 5, src: "/avatars/5.jpg", alt: "user" },
-  { id: 6, src: "/avatars/6.jpg", alt: "user" },
-  { id: 7, src: "/avatars/7.jpg", alt: "user" },
-  { id: 8, src: "/avatars/3.jpg", alt: "user" },
-  { id: 9, src: "/avatars/1.jpg", alt: "user" },
+export const serverData: Server[] = [
+  { id: 1, src: "/avatars/1.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+     },
+  { id: 2, src: "/avatars/2.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+   },
+  { id: 3, src: "/avatars/3.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+   },
+  { id: 4, src: "/avatars/4.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+   },
+  { id: 5, src: "/avatars/5.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+   },
+  { id: 6, src: "/avatars/6.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+   },
+  { id: 7, src: "/avatars/7.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+   },
+  { id: 8, src: "/avatars/3.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+   },
+  { id: 9, src: "/avatars/1.jpg", serverName: 'Sanzu &apos; shit',
+    categories: [{
+      name: 'Welcome',
+      channels: [
+        {
+          id: 1,
+          name: 'Welcome to',
+          description: 'well',
+          messages: 'sip sip'
+        }
+      ]
+    }]
+   },
 ];
 
 export const PlusIcon = PiPlus;
